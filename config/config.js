1ddf6bee3e96772c6ba752c4ff4eb4c139d8f8cc
Andrii <kotsupyr.a@gmail.com>
2022-02-09 09:57:24 +0200

    setup db config

---
+++ config/config.js
@@ -0,0 +1,45 @@
+require("dotenv-safe").config({
+  allowEmptyValues: true,
+});
+
+module.exports = {
+  development: {
+    use_env_variable: "DATABASE_URL",
+    migrationStorageTableName: "sequelize_meta",
+    seederStorage: "sequelize",
+    seederStorageTableName: "sequelize_data",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
+  test: {
+    use_env_variable: "DATABASE_URL",
+    migrationStorageTableName: "sequelize_meta",
+    seederStorage: "sequelize",
+    seederStorageTableName: "sequelize_data",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
+  production: {
+    use_env_variable: "DATABASE_URL",
+    migrationStorageTableName: "sequelize_meta",
+    seederStorage: "sequelize",
+    seederStorageTableName: "sequelize_data",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  },
+};
