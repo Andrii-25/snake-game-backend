0d9818808f9d9912c44779f2b302be486e77c228
Andrii <kotsupyr.a@gmail.com>
2022-02-09 13:20:52 +0200

    add token model and migration

---
+++ models/token.js
@@ -0,0 +1,29 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Token extends Model {
+    static associate(models) {
+      this.hasOne(models.User, {
+        foreignKey: "user",
+        otherKey: "id",
+      });
+    }
+  }
+  Token.init(
+    {
+      user: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: "User",
+          key: "id",
+        },
+      },
+      refreshToken: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Token",
+    }
+  );
+  return Token;
+};
