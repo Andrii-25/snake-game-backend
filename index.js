3a8b34ed5bbe0c06cfc407dc3fbdd062ea558948
Andrii <kotsupyr.a@gmail.com>
2022-02-12 15:48:47 +0200

    enable all cors requests

--- index.js
+++ index.js
@@ -9,12 +9,14 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    credentials: true,
-    origin: process.env.CLIENT_URL,
-  })
-);
+// app.use(
+//   cors({
+//     credentials: true,
+//     origin: process.env.CLIENT_URL,
+//   })
+// );
+
+app.use(cors());
 app.use("/api", router);
 app.use(errorMiddleware);
 
